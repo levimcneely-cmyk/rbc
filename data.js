a85b00c05b4ffeaedabf2cdfeda1e60682f70490
LMac <levi.mcneely@gmail.com>
2026-09-02 19:26:53 -0400

    Add historyCsvUrl to CONFIG object

--- data.js
+++ data.js
@@ -74,6 +74,7 @@ const WORSHIP_SONGS = [
 const CONFIG = {
   teamsCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEr3OYy-lPwKzA-VEfJkTJf-3s6HMEaC57gGQGnIhdT6RS9c8Xr0z2Dz8lW1tSBWuf7i0M7HcoiedI/pub?output=csv",
   resultsCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEr3OYy-lPwKzA-VEfJkTJf-3s6HMEaC57gGQGnIhdT6RS9c8Xr0z2Dz8lW1tSBWuf7i0M7HcoiedI/pub?gid=1672636483&single=true&output=csv",
+  historyCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEr3OYy-lPwKzA-VEfJkTJf-3s6HMEaC57gGQGnIhdT6RS9c8Xr0z2Dz8lW1tSBWuf7i0M7HcoiedI/pub?gid=880893577&single=true&output=csv",
 
   // Points awarded for each finishing place, by index (1st, 2nd, 3rd...).
   // Any place beyond this list earns the last value.
